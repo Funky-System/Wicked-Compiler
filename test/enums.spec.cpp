3d3ad8bc1bb42541bbc19db7f63ea0af96c42204
Bas de Ruijter <Bas@Funky-System.org>
2018-02-18 16:28:20 +0100

    enum test

--- test/enums.spec.cpp
+++ test/enums.spec.cpp
@@ -1,4 +1,24 @@
-//
-// Created by Bas du Pré on 18-02-18.
-//
+#include "gtest/gtest.h"
+#include "frontend.h"
+
+TEST(Enums, Enums) {
+auto raw_str = R"wckd(
+        import testlib
+
+        enum TestEnum
+            Var1, var, var2 = 30, Hoi,
+            bla,
+            test = 1
+        end
+
+        testlib.assert(TestEnum.Var1 == 0)
+        testlib.assert(TestEnum.var == 1)
+        testlib.assert(TestEnum.var2 == 30)
+        testlib.assert(TestEnum.Hoi == 31)
+        testlib.assert(TestEnum.bla == 32)
+        testlib.assert(TestEnum.test == 1)
+    )wckd";
+
+compile_and_run(raw_str);
+}
 
